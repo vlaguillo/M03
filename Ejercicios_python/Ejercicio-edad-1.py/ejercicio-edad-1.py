c5c888775f101d5bb2e11d925972ddaf9fdedd97
Víctor Laguillo <vlaguillo@localhost.localdomain>
2017-03-02 20:08:26 +0100

    Deures

---
+++ Ejercicios_python/Ejercicio-edad-1.py/ejercicio-edad-1.py
@@ -0,0 +1,8 @@
+#coding: utf8
+edad = input("Indica la teva edat: ")
+
+if edad >=15 and edad <=17:
+	print "Pots entrar a la sesió de tarda"
+	
+else:
+	print "No pots entrar a la sesió de tarda"
---
+++ Ejercicios_python/Ejercicio-edad-2.py/ejercicio-edad-2.py
@@ -0,0 +1,8 @@
+#coding: utf8
+edad = input("Indica la teva edat: ")
+
+if edad >= 18 and edad <=23:
+	print "Pots entrar a la sesió de joves"
+	
+else:
+	print "No pots entrar"
