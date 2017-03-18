005262e67d19c933e5cce70e7acfadb9c78e9381
Víctor Laguillo <vlaguillo@localhost.localdomain>
2017-03-18 19:57:22 +0100

    Correción

--- Ejercicios_python/Deberes_17-03/ejercicio-mayor.py
+++ Ejercicios_python/Deberes_17-03/ejercicio-mayor.py
@@ -6,8 +6,9 @@ num2 = input("Indique el segundo numero: ")
 if (num1 < num2):
 	print num1,"es mas pequeño que",num2
 	
-elif (num1 > num2):
-	print num1,"es mas grande que",num2
-	
 else:
-	print num1,"y",num2,"son iguales"
+	if (num1 > num2):
+		print num1,"es mas grande que",num2
+	
+	else:
+		print num1,"y",num2,"son iguales"
