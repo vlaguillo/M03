b036fb463cc1aed32a48080a68a5baa21b71480c
Víctor Laguillo <vlaguillo@localhost.localdomain>
2017-04-06 21:23:46 +0200

    Deures

---
+++ Ejercicios_python/Carta_mes_alta_II.py/carta_mes_alta_II.py
@@ -0,0 +1,62 @@
+# Coding: UTF-8
+sortir=False
+import os
+from random import randint
+os.system('clear')
+while (sortir==False):
+	J1n=randint(2,14)
+	J1p=randint(1,4)
+	J2n=randint(2,14)
+	J2p=randint(1,4)
+
+	numero1=J1n
+	if (J1n==11):
+		numero1="J"
+	if (J1n==12):
+		numero1="Q"
+	if (J1n==13):
+		numero1="K"
+	if (J1n==14):
+		numero1="A"
+
+	if (J1p==1):
+		pal="picas"
+	if (J1p==2):
+		pal="trebols"
+	if (J1p==3):
+		pal="diamantes"
+	if (J1p==4):
+		pal="corazones"
+		
+	numero2=J2n
+	if (J2n==11):
+		numero2="J"
+	if (J2n==12):
+		numero2="Q"
+	if (J2n==13):
+		numero2="K"
+	if (J2n==14):
+		numero2="A"
+
+	if (J2p==1):
+		pal="picas"
+	if (J2p==2):
+		pal="treboles"
+	if (J2p==3):
+		pal="diamantes"
+	if (J2p==4):
+		pal="corazones"
+
+	print "J1:",numero1,"de",pal
+	
+	print "J2:",numero2,"de",pal
+
+	if (J1n==J2n):
+		print "Hay un empate"
+	else:
+		if (J1n>J2n):
+			print "Gana el J1"
+			sortir=True
+		else:
+			print "Gana J2"
+			sortir=True
