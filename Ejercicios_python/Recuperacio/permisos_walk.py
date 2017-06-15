11c224f69b7f415fed19fa72ef3f220ddd85d623
Víctor Laguillo <vlaguillo@localhost.localdomain>
2017-06-15 22:22:45 +0200

    Recuperació

--- Ejercicios_python/Recuperacio/permisos_walk.py
+++ Ejercicios_python/Recuperacio/permisos_walk.py
@@ -4,12 +4,11 @@ import os
 import stat
  
 path_to_explore="./test"
-        
-# Mostrem ruta tot    
+            
 for root, dirs, files in os.walk(path_to_explore):
     for name in files:
         ruta_completa=os.path.join(root, name)
         os.chmod(ruta_completa,0660)
-        print(ruta_completa) ,
-        permissions = stat.S_IMODE (os.stat (ruta_completa).st_mode)
-        print oct(permissions)
+        print ruta_completa ,
+        permisos = stat.S_IMODE (os.stat (ruta_completa).st_mode)
+        print oct(permisos)
